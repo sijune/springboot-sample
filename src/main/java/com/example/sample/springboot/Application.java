80dc864d70d770e6ecebe1158105ecdeb08c7270
sijune <sijune0525@likelion.org>
2020-03-03 18:03:10 +0900

    add security func

--- src/main/java/com/example/sample/springboot/Application.java
+++ src/main/java/com/example/sample/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //Jpa Auditing 활성화
+//@EnableJpaAuditing //Jpa Auditing 활성화
 @SpringBootApplication
 //스프링 부트의 자동 설정, 스프링 빈읽기와 생성을 모두 자동으로 설정
 
--- src/test/java/com/example/sample/springboot/web/HelloControllerTest.java
+++ src/test/java/com/example/sample/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.sample.springboot.web;
 
+import com.example.sample.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +16,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+            @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
@@ -21,6 +28,7 @@ public class HelloControllerTest {
     //해당 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 가능
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -30,6 +38,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
